04401e44d34dda59e3d8e2a3840fab5a790bda7a
shreyd04 <officialsd27204@gmail.com>
2026-01-16 18:03:08 +0530

    Add initial profit_margin.sql file for calculating profit margins

--- Yearly_Organise.sql
+++ Yearly_Organise.sql
@@ -1,10 +1,20 @@
 CREATE TABLE Revenue_By_Year AS
-SELECT  
-EXTRACT(YEAR FROM order_date) AS sales_year,
+SELECT 
+year,
+--The "Ship Date Fallback" (The Finance Fix)
+--EXTRACT(year FROM COALESCE(order_date , ship_date)) AS sales_year,
 SUM(sales) AS total_sales
 FROM SS_sales
 GROUP BY 
-sales_year
-ORDER BY sales_year ASC;
+year
+ORDER BY year ASC;
+
+DROP TABLE IF EXISTS Revenue_By_Year;
 
 SELECT * FROM Revenue_By_Year;
+SELECT
+SUM(sales) AS total_revenue,
+year
+FROM temp_sales
+GROUP BY year
+ORDER BY year ASC;
